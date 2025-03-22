c662cdb7f41252d5e1a1eee465d25298fa5a4bda
Job Oboth <trappsavage601@gmail.com>
2025-03-23 00:25:11 +0300

    created new files and modified some

---
+++ week03/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("lastModified").textContent = 
+"Last Modified: " + document.lastModified;
+
+// Display Current Date
+document.getElementById("current-date").textContent = new Date().toDateString();
